7f12efe2f449af5c20d9df333ca9c84a48f17165
David Freau <dfreau@gmail.com>
2020-07-22 11:34:44 +0200

    ticetac

---
+++ models/bdd.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// useNewUrlParser ;)
+var options = {
+  connectTimeoutMS: 5000,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+ };
+
+ var identifiants = {
+   mdp: 'MOT DE PASSE',
+   identifiant: 'IDENTIFIANT',
+   nomCluster: 'CLUSTER'
+ }
+
+ 
+
+// --------------------- BDD -----------------------------------------------------
+mongoose.connect(`mongodb+srv://${identifiants.identifiant}:${identifiants.mdp}@${identifiants.nomCluster}.mongodb.net/Ticketac?retryWrites=true`,
+   options,
+   function(err) {
+    if (err) {
+      console.log(`error, failed to connect to the database because --> ${err}`);
+    } else {
+      console.info('*** Database Ticketac connection : Success ***');
+    }
+   }
+);
